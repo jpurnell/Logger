1f1b9930c0287cc3effe98cd3f12e9063d0fadb3
Justin Purnell <jpurnell@users.noreply.github.com>
2024-02-09 23:49:25 -0500

    Minor cleanup

--- Sources/Logger/Logger.swift
+++ Sources/Logger/Logger.swift
@@ -1,2 +1,16 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import OSLog
+
+@available(macOS 11.0, *)
+extension Logger {
+	init(_ function: String, _ filePath: String = #file, _ depth: Int = 3, _ separator: String = " > ") {
+		let fileName = filePath.components(separatedBy: "/").suffix(depth).joined(separator: separator)
+		if let bundleIdentifier = Bundle.main.bundleIdentifier {
+			self.init(subsystem: "\(bundleIdentifier)\(bundleIdentifier != "" ? separator : "")\(fileName)", category: "\(function)")
+		} else {
+			self.init(subsystem: "\(fileName)", category: "\(function)")
+		}
+	}
+}
